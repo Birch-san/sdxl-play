41a34c17d212614527b97aa1d7cea6978d5ce7c5
Alex Birch <Birch-san@users.noreply.github.com>
2023-07-14 00:07:23 +0100

    some ideas on how to relate to a cache when encoding prompts for each batch, but not finished and feels like a dead end.

--- scripts/in_between_test.py
+++ scripts/in_between_test.py
@@ -1,11 +1,12 @@
 from dataclasses import dataclass
 from easing_functions import CubicEaseInOut
-from typing import List, Callable, TypeAlias
+from typing import List, Callable, TypeAlias, Set, Dict, Optional
 
 from src.interpolation.intersperse_linspace import intersperse_linspace, InterpSpec
 from src.interpolation.in_between import ManneredInBetweenParams
 from src.interpolation.inter_prompt import InterPrompt
 from src.interpolation.interp_strategy import InterpStrategy
+from src.iteration.batched import batched
 
 QuotientModifier: TypeAlias = Callable[[float], float]
 
@@ -23,7 +24,7 @@ keyframes: List[str] = [
 quotient_modifiers: List[QuotientModifier] = [lambda x:x, CubicEaseInOut()]
 
 interp_specs: List[InterpSpec[InterpManner]] = [InterpSpec[InterpManner](
-  steps=5,
+  steps=3,
   manner=InterpManner(
     quotient_modifier=modifier,
     strategy=InterpStrategy.Slerp,
@@ -40,7 +41,7 @@ def make_inbetween(params: ManneredInBetweenParams[str, InterpManner]) -> InterP
     strategy=params.manner.strategy,
   )
 
-cond_linspace: List[str|InterPrompt] = intersperse_linspace(
+frames: List[str|InterPrompt] = intersperse_linspace(
   keyframes=keyframes,
   make_inbetween=make_inbetween,
   interp_specs=interp_specs,
@@ -48,3 +49,40 @@ cond_linspace: List[str|InterPrompt] = intersperse_linspace(
 
 # friendly place to put a breakpoint
 pass
+
+
+
+max_batch_size=2
+
+encodings: Dict[str, str] = {'old': 'OLD'}
+
+def register_prompt_text(prompt_text: str) -> None:
+  if prompt_text not in prompt_text_to_ix:
+    prompt_text_to_ix[prompt_text] = len(prompt_texts_ordered)
+    prompt_texts_ordered.append(prompt_text)
+  ix: int = prompt_text_to_ix[prompt_text]
+  return ix
+
+def encode(prompts: List[str]) -> List[str]:
+  return [prompt.upper() for prompt in prompts]
+
+for batch_ix, batch_frames in enumerate(batched(frames, max_batch_size)):
+  prompt_text_to_ix: Dict[str, int] = {}
+  prompt_texts_ordered: List[str] = []
+  prompt_text_instance_ixs: List[List[int]] = []
+  for frame in batch_frames:
+    sample_prompt_text_instance_ixs: List[int] = []
+    sample_prompts: List[str] = [frame.from_, frame.to] if isinstance(frame, InterPrompt) else [frame]
+    for prompt in sample_prompts:
+      prompt_ix: int = register_prompt_text(prompt)
+      sample_prompt_text_instance_ixs.append(prompt_ix)
+    prompt_text_instance_ixs.append(sample_prompt_text_instance_ixs)
+  
+  existing_prompts: Set[str] = encodings.keys() & prompt_text_to_ix.keys()
+  new_prompts: Set[str] = prompt_text_to_ix.keys() - existing_prompts
+  new_encoded: List[str] = encode(new_prompts)
+  encodings = {
+    **{key: encodings[key] for key in encodings if key in existing_prompts},
+    **{key: value for key, value in zip(new_prompts, new_encoded)},
+  }
+  pass
