c0c2ada36dfe0420b1e4350818fbb5d5db4e0600
Alex Birch <Birch-san@users.noreply.github.com>
2023-07-13 22:29:59 +0100

    support modifying interp quotient via (stateful) easing function

--- scripts/in_between_test.py
+++ scripts/in_between_test.py
@@ -1,14 +1,17 @@
 from dataclasses import dataclass
+from easing_functions import CubicEaseInOut
+from typing import List, Callable, TypeAlias
 
 from src.interpolation.intersperse_linspace import intersperse_linspace, InterpSpec
 from src.interpolation.in_between import ManneredInBetweenParams
 from src.interpolation.inter_prompt import InterPrompt
 from src.interpolation.interp_strategy import InterpStrategy
 
-from typing import List
+QuotientModifier: TypeAlias = Callable[[float], float]
 
 @dataclass
 class InterpManner:
+  quotient_modifier: QuotientModifier
   strategy: InterpStrategy
 
 keyframes: List[str] = [
@@ -17,22 +20,27 @@ keyframes: List[str] = [
   'geese',
 ]
 
+quotient_modifiers: List[QuotientModifier] = [lambda x:x, CubicEaseInOut()]
+
 interp_specs: List[InterpSpec[InterpManner]] = [InterpSpec[InterpManner](
-  steps=3,
+  steps=5,
   manner=InterpManner(
+    quotient_modifier=modifier,
     strategy=InterpStrategy.Slerp,
   ),
-)] * (len(keyframes)-1)
+) for _, modifier in zip(range(len(keyframes)-1), quotient_modifiers)]
 
 def make_inbetween(params: ManneredInBetweenParams[str, InterpManner]) -> InterPrompt:
+  # apply easing function
+  modified_quotient: float = params.manner.quotient_modifier(params.quotient)
   return InterPrompt(
     from_=params.from_,
     to=params.to,
-    quotient=params.quotient,
+    quotient=modified_quotient,
     strategy=params.manner.strategy,
   )
 
-cond_linspace: List[InterPrompt] = intersperse_linspace(
+cond_linspace: List[str|InterPrompt] = intersperse_linspace(
   keyframes=keyframes,
   make_inbetween=make_inbetween,
   interp_specs=interp_specs,
