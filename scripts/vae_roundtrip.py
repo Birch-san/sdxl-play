3a363febb7a6e05ac88cb76d31abd386a3d445f0
Alex Birch <Birch-san@users.noreply.github.com>
2023-11-07 00:21:43 +0000

    measure memory used by VAE attn impl

--- scripts/vae_roundtrip.py
+++ scripts/vae_roundtrip.py
@@ -8,6 +8,7 @@ from diffusers.models.vae import DecoderOutput, DiagonalGaussianDistribution
 from PIL import Image
 import numpy as np
 from typing import Literal
+from time import perf_counter
 
 from src.attn.null_attn_processor import NullAttnProcessor
 from src.attn.natten_attn_processor import NattenAttnProcessor
@@ -31,6 +32,7 @@ vae: AutoencoderKL = AutoencoderKL.from_pretrained(
   **vae_kwargs,
 )
 
+natten_kernel=15
 attn_impl: Literal['natten', 'null', 'original'] = 'natten'
 match(attn_impl):
   case 'natten':
@@ -40,7 +42,7 @@ match(attn_impl):
     # I haven't checked what's the smallest kernel size that can look identical.
     # should save you loads of memory and compute
     # (neighbourhood attention costs do not exhibit quadratic scaling with sequence length)
-    vae.set_attn_processor(NattenAttnProcessor(kernel_size=17))
+    vae.set_attn_processor(NattenAttnProcessor(kernel_size=natten_kernel))
   case 'null':
     for attn in [*vae.encoder.mid_block.attentions, *vae.decoder.mid_block.attentions]:
       # you won't be needing these
@@ -57,6 +59,11 @@ match(attn_impl):
 # vae.enable_slicing() # you probably don't need this any more
 vae.eval().to(device)
 
+alloc = torch.cuda.memory_allocated(0)
+print(f'{alloc/1024**2:.2f} MiB allocated')
+alloc_plus_reserved = torch.cuda.memory_reserved(0)
+print(f'{alloc_plus_reserved/1024**2:.2f} MiB allocated+reserved')
+
 def load_img(path) -> FloatTensor:
   image: Image.Image = Image.open(path).convert("RGB")
   w, h = image.size
@@ -69,17 +76,51 @@ def load_img(path) -> FloatTensor:
   img_tensor: FloatTensor = img_tensor.permute(2, 0, 1).unsqueeze(0)
   img_tensor: FloatTensor = img_tensor / 127.5 - 1.0
   return img_tensor
-img: FloatTensor = load_img('in.jpg')
+# img: FloatTensor = load_img('in.jpg').to(device, vae.dtype)
+img: FloatTensor = load_img('out_hpf_interp_c3/00003_base_masterpiece_s42_t0.4285714626312256.jpg').to(device, vae.dtype)
 
 generator = torch.Generator(device='cpu')
 generator.manual_seed(42)
 
+iterations = 1
+batch_size = 1
+img = img.expand(batch_size, *img.shape[1:])
+
+print('initial:')
+alloc = torch.cuda.memory_allocated(0)
+print(f'{alloc/1024**2:.2f} MiB allocated')
+alloc_plus_reserved = torch.cuda.memory_reserved(0)
+print(f'{alloc_plus_reserved/1024**2:.2f} MiB allocated+reserved')
+
+torch.cuda.synchronize()
+tic = perf_counter()
 with inference_mode():
-  encoded: AutoencoderKLOutput = vae.encode(img.to(device, vae.dtype))
-  dist: DiagonalGaussianDistribution = encoded.latent_dist
-  latents: FloatTensor = dist.sample(generator=generator)
-  decoder_out: DecoderOutput = vae.decode(latents)
+  for _ in range(iterations):
+    encoded: AutoencoderKLOutput = vae.encode(img)
+    dist: DiagonalGaussianDistribution = encoded.latent_dist
+    latents: FloatTensor = dist.sample(generator=generator)
+    print('after encoding, sampling:')
+    alloc = torch.cuda.memory_allocated(0)
+    print(f'{alloc/1024**2:.2f} MiB allocated')
+    alloc_plus_reserved = torch.cuda.memory_reserved(0)
+    print(f'{alloc_plus_reserved/1024**2:.2f} MiB allocated+reserved')
+    decoder_out: DecoderOutput = vae.decode(latents)
+    torch.cuda.synchronize()
+duration_secs = perf_counter() - tic
+secs_per_sample = duration_secs/(iterations*batch_size)
+samples_per_sec = secs_per_sample**-1
+print(f'{iterations} batches of {batch_size}:')
+print(f'{secs_per_sample:.2f} secs/sample')
+print(f'{samples_per_sec:.2f} samples/sec')
+alloc = torch.cuda.memory_allocated(0)
+print(f'{alloc/1024**2:.2f} MiB allocated')
+alloc_plus_reserved = torch.cuda.memory_reserved(0)
+print(f'{alloc_plus_reserved/1024**2:.2f} MiB allocated+reserved')
+# original
+# 20 batches of 8:
+# 0.26 secs/sample
+# 3.87 samples/sec
 
-sample: FloatTensor = decoder_out.sample.div(2).add_(.5).clamp_(0,1)
+sample: FloatTensor = decoder_out.sample[:1].div(2).add_(.5).clamp_(0,1)
 
-save_image(sample, f'out.{attn_impl}.png')
+save_image(sample, f'out_roundtrip/1.{attn_impl}{natten_kernel}.png')
